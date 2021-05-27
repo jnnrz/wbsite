0ac6a2b01e4ef1319c17e2eef8e2bbd62c9e2fcc
Johann Perozo <johann.prz@gmail.com>
2021-05-27 15:16:29 -0400

    Adds navbar and hero to home page

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 ﻿export { default as Head } from "./Head";
 export { default as Button } from "./Button";
 export { default as Container } from "./Container";
+export { default as Navbar } from "./Navbar";
+export { default as Hero } from "./Hero";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 ﻿import { NextPage } from "next";
-import { Container } from "@components/index";
+import { Container, Hero, Navbar } from "@components/index";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="flex flex-col">
+      <Navbar />
+      <Hero />
       <Container></Container>
-    </>
+    </div>
   );
 };
 
